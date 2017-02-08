7462cc91ee16dbd455171335130c05d613d5691c
Xyrephon <jtuc1613@740s-pl-115.local>
2017-02-08 09:45:55 -0700

    Created a method in the Controller to test the IntNodeArray.

--- MegaProject/Controller/StructureController.cpp
+++ MegaProject/Controller/StructureController.cpp
@@ -5,5 +5,30 @@
 //  Created by Tucker, Jonah on 2/8/17.
 //  Copyright © 2017 Tucker, Jonah. All rights reserved.
 //
-
+#include "IntNodeArray.hpp"
 #include "StructureController.hpp"
+#include <iostream>
+
+using namespace std;
+
+StructureController :: StructureController()
+{
+    
+}
+
+void StructureController :: testIntArray()
+{
+    cout << "Creating an IntNodeArray" << endl;
+    IntNodeArray temp = IntNodeArray(3);
+    for(int spot = 0; spot < 3; spot ++)
+    {
+        cout << temp.getFromIndex(spot) << " is at " << spot << endl;
+    }
+}
+
+void StructureController :: start()
+{
+    cout << "Going to test the IntNodeArray" << endl;
+    testIntArray();
+    cout << "Finished Int ArrayNode testing" << endl;
+}
