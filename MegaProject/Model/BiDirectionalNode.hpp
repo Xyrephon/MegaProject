430bc3f869d7a2cd1c79027ddefcb687a7e98a4f
Xyrephon <jtuc1613@740s-pl-115.local>
2017-02-27 09:38:50 -0700

    Created BiDirectionalNode

---
+++ MegaProject/Model/BiDirectionalNode.hpp
@@ -0,0 +1,18 @@
+//
+//  BiDirectionalNode.hpp
+//  MegaProject
+//
+//  Created by Tucker, Jonah on 2/27/17.
+//  Copyright © 2017 Tucker, Jonah. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_h
+#define BiDirectionalNode_h
+
+template <class Type>
+class BIDirectionalNode : public Node<Type>
+{
+public:
+private:
+};
+#endif /* BiDirectionalNode_h */
