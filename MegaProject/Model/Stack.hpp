43d642eae85374e6077b7eed41790fc92fa558ea
Xyrephon <jtuc1613@740s-pl-115.local>
2017-03-03 09:32:53 -0700

    Made the push method in stack.

--- MegaProject/Model/Stack.hpp
+++ MegaProject/Model/Stack.hpp
@@ -35,4 +35,29 @@ void Stack<Type> :: add(Type valueToAdd)
     push(valueToAdd);
 }
 
+
+/*
+Adds the supplied object to the stack to the end.
+ Set new object to point to end.
+ Increases the size by 1.
+ Adjusts the end pointer to reflect the new end of the stack.
+*/
+template <class Type>
+void Stack<Type> :: push(Type addThing)
+{
+    BiDirectionalNode<Type> * addToStack = new BiDirectionalNode(addedThing);
+ 
+    if(this->size == 0 || this->front == nullptr || this->end == nullptr)
+    {
+        this->front = addToStack;
+    }
+    else
+    {
+        this->end->setNextPointer(addToStack);
+        addToStack->setPreviousPointer(this->end);
+    }
+    this->end = addToStack;
+    this->size++;
+}
+
 #endif /* Stack_h */
