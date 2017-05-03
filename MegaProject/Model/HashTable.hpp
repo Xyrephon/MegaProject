2774b117168a907003afbd5fad0240e4697865f2
Xyrephon <jtuc1613@740s-pl-115.local>
2017-05-03 09:14:27 -0600

    Updated constructor of HashTable.

--- MegaProject/Model/HashTable.hpp
+++ MegaProject/Model/HashTable.hpp
@@ -41,6 +41,7 @@ HashTable<Type> :: HashTable()
     this->efficiencyPercentage = .667;
     this->size = 0;
     this->hashTableStorage = HashNode<Type> * [capacity];
+    std :: fill_n(hashTableStorage, capacity, nullptr);
 }
 
 template <class Type>
